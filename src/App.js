e17cf15cd6c3a049f79788472cf9b32a1c36e376
ahmed <ahmed.akermi@ISAEG.u-gafsa.tn>
2022-10-02 19:29:02 +0100

    firdtcommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import MuiFormikYup from "./components/MuiFormikYup";
+import SurveyComponent from "./components/SurveyComponent";
+import SurveyMultipage from "./components/SurveyMultipage";
+import SurveyCreatorComponent from "./components/SurveyCreatorComponent";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SurveyCreatorComponent />
+      <SurveyComponent />
+      <SurveyMultipage />
+      <MuiFormikYup />
     </div>
   );
 }
